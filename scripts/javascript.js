3f6be7dc581ae4ac68e6032336bacbf8ed7575d8
Sophia <sophiareme110@gmail.com>
2022-07-20 11:51:29 -0500

    new changes w2

---
+++ scripts/javascript.js
@@ -0,0 +1,95 @@
+// this is a comment 
+
+
+/*
+
+this
+ is 
+ a
+  comment 
+  
+  */
+
+  var myName;
+
+  // no spaces and cant start with a number (for vars)
+
+  var a;
+  a = 47;
+  //console.log(a);
+  //console.log(*this is a*);
+  
+
+  var b;
+  b = a
+  console.log("b is equal to:" + b);
+
+  //assignment operator
+
+  var c = 0;
+
+  var myfirstname = "sophia";
+  var mylastname = "reme";
+  var num = "12";
+
+  var number = 23; 
+
+  var x = 15;
+  var y = 90;
+  var z = "pink";
+
+  x = x + 1;
+  y = y + 5;
+  z = z + " coder";
+
+  console.log(x);
+  console.log(y);
+  console.log(z);
+
+
+var camper = "sophia";
+var camper = "sandra";
+console.log(camper);
+
+let student = "sophia"
+//let student = "sophia"
+console.log(student);
+
+const FAV_PET = "cats";
+//FAV_PET = "dogs";
+
+const SUM = 30 + 30;
+const DIFFERENCE = 50-20;
+const QUOTIENT = 44 / 12;
+const PRODUCT =  8 * 10;
+console.log(SUM);
+console.log(DIFFERENCE);
+console.log(QUOTIENT);
+console.log(PRODUCT);
+
+let newvar = 47;
+newvar = newvar + 1;
+//equivalent to...
+newvar ++;
+console.log(newvar);
+
+newvar = newvar - 1;
+//same as..
+newvar--;
+
+let v = 1;
+v += 5;
+console.log("this is v: " + v);
+
+let s = 10
+s-= 5;
+console.log("this is s:" +5);
+
+let m = 5;
+m*= 2;
+console.log("this is m:" + m);
+
+let d = 10
+d/= 2;
+console.log("this is d: " + d);
+
